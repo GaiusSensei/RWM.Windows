ca149d6fb9a9424d52db6e5778843683b1a0daed
Erik Gaius Capistrano <gaius@about.me>
2013-07-24 09:17:46 +0000

    Initial Commit.

---
+++ Scripts/site.js
@@ -0,0 +1,9 @@
+head.js(
+    { phonegap: "cordova.js" },
+    { jquery: "Scripts/jQuery/jquery-1.10.2.min.js" },
+    { tstore: "Scripts/jQuery/jquery.total-storage.min.js" },
+    { bstrap: "Scripts/jQuery/bootstrap.min.js" },
+    function readyF() {
+        // Site-wide OnDocumentLoad Javascript
+    }
+);
