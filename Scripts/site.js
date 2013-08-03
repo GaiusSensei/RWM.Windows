4dd974c3542f7fc5568b8c86fe118b2e679ef3e9
Erik Gaius Capistrano <gaius@about.me>
2013-08-03 07:03:22 +0000

    Add BootMetro Bootstrap mixin.

--- Scripts/site.js
+++ Scripts/site.js
@@ -3,6 +3,14 @@ head.js(
     { jquery: "Scripts/jQuery/jquery-1.10.2.min.js" },
     { tstore: "Scripts/jQuery/jquery.total-storage.min.js" },
     { bstrap: "Scripts/jQuery/bootstrap.min.js" },
+    "Scripts/jQuery/bootmetro.min.js",
+    "Scripts/jQuery/bootmetro-charms.min.js",
+    "Scripts/jQuery/bootmetro-icons-ie7.min.js",
+    "Scripts/jQuery/bootmetro-panorama.min.js",
+    "Scripts/jQuery/bootmetro-pivot.min.js",
+    "Scripts/jQuery/bootstrap-datepicker.js",
+    "Scripts/jQuery/jquery.mousewheel.min.js",
+    "Scripts/jQuery/jquery.touchSwipe.min.js",
     function readyF() {
         // Site-wide OnDocumentLoad Javascript
     }
